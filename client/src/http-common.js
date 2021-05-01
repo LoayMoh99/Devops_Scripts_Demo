7b9b1f9e9c26ee517854bc955ff454c2e9c83fed
Loay Mohamed <loaymoh99@loaymoh99-HP-Pavilion-15-Notebook-PC>
2021-05-02 01:54:02 +0200

    routing edited elmafrood

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:81/api",
+  baseURL: "http://dropoidsdemo-env.eba-cpd49qay.us-east-2.elasticbeanstalk.com/api",
   headers: {
     "Content-type": "application/json"
   }
